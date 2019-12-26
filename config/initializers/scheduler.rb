3fb5660bcaaa96db08ce54dad91d6771b96ca605
longpoll <uzakkk777@ya.ru>
2019-12-26 13:32:53 +0300

    fixed tests and scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,14 +4,13 @@ scheduler = Rufus::Scheduler.new
 
 # Рассылка новостей в первый день каждой недели
 
-#scheduler.cron '00 07 * * 01' do 
-scheduler.cron '* * * * *' do 
+scheduler.cron '00 07 * * 01' do 
 	UserMailer.with(everyDay: 0).news_digest.deliver_later
 end
 
 
 # Рассылка новостей каждый день в 8 утра
 
-scheduler.cron '* * * * *' do
+scheduler.cron '00 08 * * *' do
 	UserMailer.with(everyDay: 1).news_digest.deliver_later
 end
--- spec/request/root_spec.rb
+++ spec/request/root_spec.rb
@@ -1,19 +1,11 @@
 require "rails_helper"
 
 
-#RSpec.describe "App", :type => :request do
+RSpec.describe "App", :type => :request do
 
-#	include Capybara::DSL
-
-#  it "create news not auth user redirected" do
-#  	get new_news_path
-#    expect(response).to have_http_status(302)
-#  end
-#  it "user login" do
-#  	post user_session_path, :params => {
-#  		:user_email => "uzakkk777@ya.ru",
-#  		:user_password => "rostov97"
-#  	}
-#  	ap response
-#  end 
-#end
+  it "create news not auth user redirected" do
+  	get new_news_path
+    expect(response).to have_http_status(302)
+  end
+  
+end
